48f684465c58ad14d36dd69c0c0d2dd732fc9806
Roberto Previdi <hariseldon78@gmail.com>
2018-10-15 01:56:32 +0200

    layout roby10 https://configure.ergodox-ez.com/layouts/E55Q/latest/3

---
+++ keyboards/ergodox_ez/keymaps/hariseldon78/config.h
@@ -0,0 +1,8 @@
+/*
+  Set any config.h overrides for your specific keymap here.
+  See config.h options at https://docs.qmk.fm/#/config_options?id=the-configh-file
+*/
+#undef MOUSEKEY_TIME_TO_MAX
+#undef MOUSEKEY_MAX_SPEED
+#define MOUSEKEY_TIME_TO_MAX    120
+#define MOUSEKEY_MAX_SPEED      3
