183b1f23e3c4e6e21903446a1d4dd3649e63417c
Roberto Previdi <hariseldon78@gmail.com>
2018-10-15 02:37:14 +0200

    refined mouse wheel speed

--- keyboards/ergodox_ez/keymaps/hariseldon78/config.h
+++ keyboards/ergodox_ez/keymaps/hariseldon78/config.h
@@ -2,7 +2,17 @@
   Set any config.h overrides for your specific keymap here.
   See config.h options at https://docs.qmk.fm/#/config_options?id=the-configh-file
 */
-#undef MOUSEKEY_TIME_TO_MAX
-#undef MOUSEKEY_MAX_SPEED
-#define MOUSEKEY_TIME_TO_MAX    120
-#define MOUSEKEY_MAX_SPEED      3
+#undef MOUSEKEY_WHEEL_TIME_TO_MAX
+#define MOUSEKEY_WHEEL_TIME_TO_MAX    	120
+
+#undef MOUSEKEY_WHEEL_MAX_SPEED
+#define MOUSEKEY_WHEEL_MAX_SPEED      	8
+
+#undef MOUSEKEY_WHEEL_DELTA
+#define MOUSEKEY_WHEEL_DELTA			1
+
+#undef MOUSEKEY_WHEEL_MAX
+#define MOUSEKEY_WHEEL_MAX 				1
+
+#undef MOUSEKEY_INTERVAL
+#define MOUSEKEY_INTERVAL       		50
