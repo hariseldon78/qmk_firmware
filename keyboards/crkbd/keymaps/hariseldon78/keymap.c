e6e3953cbc1eb7ad12011f5360298073aa7c51d2
Roberto Previdi <hariseldon78@gmail.com>
2022-02-17 07:14:27 +0100

    trying to make mod tap work

--- keyboards/crkbd/keymaps/hariseldon78/keymap.c
+++ keyboards/crkbd/keymaps/hariseldon78/keymap.c
@@ -19,10 +19,10 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #include QMK_KEYBOARD_H
 #include <stdio.h>
 
-const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {[0] = LAYOUT_split_3x6_3(KC_TAB, KC_Q, KC_H, KC_O, KC_U, KC_X, KC_G, KC_C, KC_R, KC_F, KC_Z, KC_BSPC, LT(3, KC_ESC), KC_Y, KC_I, KC_E, KC_A, KC_QUOT, KC_D, KC_S, KC_T, KC_N, KC_B, KC_ENT, KC_LSFT, KC_J, KC_SLSH, KC_DOT, KC_COMM, KC_K, KC_W, KC_M, KC_L, KC_P, KC_V, TG(2), LCTL_T(KC_ESC), LALT_T(KC_SPC), KC_LGUI, LSFT_T(KC_SPC), LT(1, KC_SPC), LT(2, KC_SPC)),
-                                                              [1] = LAYOUT_split_3x6_3(KC_TRNS, KC_1, KC_2, KC_3, KC_4, KC_5, KC_6, KC_7, KC_8, KC_9, KC_0, KC_DEL, KC_TRNS, KC_EQL, KC_LPRN, KC_RPRN, KC_MINS, KC_PLUS, KC_TILD, KC_COLN, KC_LCBR, KC_RCBR, KC_SCLN, KC_TRNS, KC_TRNS, KC_PIPE, KC_LBRC, KC_RBRC, KC_DQUO, KC_GRV, KC_UNDS, KC_NO, KC_LT, KC_GT, KC_SLSH, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_NO, KC_TRNS),
-                                                              [2] = LAYOUT_split_3x6_3(KC_TRNS, KC_BTN4, KC_BTN2, KC_MS_U, KC_BTN1, KC_WH_U, KC_SLCK, KC_INS, KC_UP, KC_NO, KC_PGUP, KC_DEL, KC_TRNS, KC_BTN5, KC_MS_L, KC_MS_D, KC_MS_R, KC_WH_D, KC_CAPS, KC_LEFT, KC_DOWN, KC_RGHT, KC_PGDN, KC_TRNS, KC_TRNS, KC_NO, KC_NO, KC_BTN3, KC_WH_L, KC_WH_R, KC_PSCR, KC_HOME, KC_NO, KC_END, KC_NO, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS),
-                                                              [3] = LAYOUT_split_3x6_3(KC_NO, KC_F1, KC_F2, KC_F3, KC_F4, KC_F5, KC_F6, KC_F7, KC_F8, KC_F9, KC_F10, KC_DEL, KC_NO, KC_F11, KC_F12, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_TRNS, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_TRNS, KC_TRNS, KC_NO, KC_NO)};
+const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {[0] = LAYOUT_split_3x6_3(KC_TAB, KC_Q, KC_H, KC_O, KC_U, KC_X, KC_G, KC_C, KC_R, KC_F, KC_Z, KC_BSPC, LT(3, KC_ESC), KC_Y, LCTL_T(KC_I), LALT_T(KC_E), LSFT_T(KC_A), KC_QUOT, KC_D, LSFT_T(KC_S), LALT_T(KC_T), LCTL_T(KC_N), KC_B, KC_ENT, KC_LCBR, KC_J, KC_SLSH, KC_DOT, KC_COMM, KC_K, KC_W, KC_M, KC_L, KC_P, KC_V, TG(2), LT(2, KC_ESC), LT(1, KC_SPC), KC_LGUI, KC_SPC, LT(1, KC_SPC), LT(2, KC_SPC)),
+                                                              [1] = LAYOUT_split_3x6_3(KC_TRNS, KC_1, KC_2, KC_3, KC_4, KC_5, KC_6, KC_7, KC_8, KC_9, KC_0, KC_DEL, KC_TRNS, KC_EQL, LCTL_T(KC_LPRN), LALT_T(KC_RPRN), LSFT_T(KC_MINS), KC_PLUS, KC_TILD, LSFT(KC_COLN), LALT_T(KC_LCBR), LCTL_T(KC_RCBR), KC_SCLN, KC_TRNS, KC_TRNS, KC_PIPE, KC_LBRC, KC_RBRC, KC_ASTR, KC_GRV, KC_UNDS, KC_EXLM, KC_LT, KC_GT, KC_BSLS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_NO, KC_TRNS),
+                                                              [2] = LAYOUT_split_3x6_3(KC_TRNS, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_SLCK, KC_INS, KC_UP, KC_NO, KC_PGUP, KC_DEL, KC_TRNS, KC_NO, KC_LCTL, KC_LALT, KC_LSFT, KC_NO, KC_CAPS, KC_LEFT, KC_DOWN, KC_RGHT, KC_PGDN, KC_TRNS, KC_TRNS, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_PSCR, KC_HOME, KC_NO, KC_END, KC_NO, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS),
+                                                              [3] = LAYOUT_split_3x6_3(KC_TRNS, KC_F1, KC_F2, KC_F3, KC_F4, KC_F5, KC_F6, KC_F7, KC_F8, KC_F9, KC_F10, KC_DEL, KC_TRNS, KC_F11, LCTL_T(KC_F12), KC_LALT, KC_LSFT, KC_NO, KC_NO, KC_LSFT, KC_LALT, KC_LCTL, KC_NO, KC_TRNS, KC_TRNS, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS)};
 
 #ifdef OLED_ENABLE
 oled_rotation_t oled_init_user(oled_rotation_t rotation) {
@@ -129,6 +129,39 @@ bool oled_task_user(void) {
 bool process_record_user(uint16_t keycode, keyrecord_t *record) {
     if (record->event.pressed) {
         set_keylog(keycode, record);
+    }
+    switch (keycode) {
+        case LCTL_T(KC_LPRN):
+            if (record->tap.count && record->event.pressed) {
+                tap_code16(KC_LPRN);
+                return false;
+            }
+            break;
+
+        case LALT_T(KC_RPRN):
+            if (record->tap.count && record->event.pressed) {
+                tap_code16(KC_RPRN);
+                return false;
+            }
+            break;
+
+        case LCTL_T(KC_LCBR):
+            if (record->tap.count && record->event.pressed) {
+                uint8_t mods = get_mods();
+                tap_code16(KC_LCBR);
+                set_mods(mods | MOD_MASK_SHIFT);
+                return false;
+            }
+            break;
+
+        case LALT_T(KC_RCBR):
+            if (record->tap.count && record->event.pressed) {
+                uint8_t mods = get_mods();
+                tap_code16(KC_RCBR);
+                set_mods(mods | MOD_MASK_SHIFT);
+                return false;
+            }
+            break;
     }
     return true;
 }
