200468487744e0905f5fc7a4abb6c71b594cd27d
Roberto Previdi <hariseldon78@gmail.com>
2024-12-03 17:38:41 +0100

    added author

--- keyboards/lily58/rev1_1/config.h
+++ keyboards/lily58/rev1_1/config.h
@@ -2,6 +2,7 @@
 Copyright 2012 Jun Wako <wakojun@gmail.com>
 Copyright 2015 Jack Humbert
 Copyright 2017 F_YUUCHI
+Copyright 2023 hariseldon78@gmail.com
 
 This program is free software: you can redistribute it and/or modify
 it under the terms of the GNU General Public License as published by
